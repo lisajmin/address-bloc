ddbccc75968bbdf1f40a14d2cd786e4f9c98fc08
Lisa Min <lisajmin@gmail.com>
2017-03-29 20:30:39 -0700

    Checkpoint 6: Complete menu

--- controllers/menu-controller.rb
+++ controllers/menu-controller.rb
@@ -13,8 +13,7 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - View by Entry Number"
-    puts "6 - Exit"
+    puts "5 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -37,10 +36,6 @@ class MenuController
         read_csv
         main_menu
       when 5
-        system "clear"
-        view_entry_number
-        main_menu
-      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -80,23 +75,86 @@ class MenuController
   end
 
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+
+    match = address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
   def read_csv
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin entry_count = address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
   end
 
-  def view_entry_number
-    puts "Enter an entry number: "
-    number = gets.chomp.to_i
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
 
-    if (number <= address_book.entries.length) && (number != 0) && (number.is_a? Integer)
-      puts "#{address_book.entries[number-1].to_s}"
-      puts "Press enter to return to main menu"
-      gets.chomp
-      system "clear"
-    else
-      puts "#{number} is an invalid entry. Please try again."
-      view_entry_number
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry:"
+    puts entry
+  end
+
+  def search_submenu(entry)
+
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+    case selection
+      when "d"
+        system "clear"
+        delete_entry(entry)
+        main_menu
+      when "e"
+        edit_entry(entry)
+        system "clear"
+        main_menu
+      when "m"
+        system "clear"
+        main_menu
+      else
+        system "clear"
+        puts "#{selection} is not a valid input"
+        puts entry.to_s
+        search_submenu(entry)
     end
   end
 
@@ -111,7 +169,10 @@ class MenuController
     case selection
     when "n"
     when "d"
+      delete_entry(entry)
     when "e"
+      edit_entry(entry)
+      entry_submenu(entry)
     when "m"
       system "clear"
       main_menu
